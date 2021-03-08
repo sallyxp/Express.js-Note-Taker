7fbd5db2b70e1a7a1c27a695d815b0164d74fe15
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-08 01:39:41 +0000

    removed old server and re-wrote and replaced

--- server.js
+++ server.js
@@ -1,58 +1,99 @@
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { json } = require("express");
+const { stringify } = require("querystring");
+const uniqid = require('uniqid');     //for unique id
 
-var app = express();
-//this command lets it work both local and hosted
-var PORT = process.env.PORT || 3000;
 
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-//designates the directory/folder the server.js script as accessable as static scripts
-app.use(express.static(__dirname));
 app.use(express.json());
 
-//save a new note to array
+//Routes - easier layout to ensure consistency/clarity
+// '/' is the default
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/notes.html')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'Develop/db/db.json')));
+app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/assets/js/index.js')));
+app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/assets/css/styles.css')));
 
-app.post("/api/notes", (req, res) => {
-    var newNote = req.body;
 
-    //get the saved data
-    var noteArray = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
-    noteArray.push(newNote);
+//Saving of new note - includes the addition of new id field
+app.post('/api/notes', (req, res) => {
+    //adds new input to request body
+    const newNote = req.body;
+    newNote.id = uniqid();
+    //writes new record details to console.log
+    console.log(newNote);
 
-    var reformattedData = JSON.stringify(noteArray);
-    fs.writeFileSync(path.join(__dirname, "../db/db.json"), reformattedData);
+    //gets existing inputs
+    let db = require(path.join(__dirname, 'Develop/db/db.json'));
+     //adds new note record to newNote array
+    db.push(newNote);
 
-    //the actual contents of the response doesn't matter in this case
-    //as it just needs a response so the function's Promises will run
-    res.send("Success");
+    //shows current note records to console
+    console.log(db);
+
+    //stringify - converts the object to a string
+    const writeFile = JSON.stringify(db, null, 2);
+
+    //adds to db.json
+    generateNotes(writeFile);
+
+    //loads notes
+    return res.json(db);
+
 });
 
-//Routes
+//Deletion of selected note
+    app.delete('/api/notes/:id', (req, res) => {
+    //selected input to request body
+    const selectedNote = req.params.id;
 
-//Basic route that gets the saved notes
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../db/db.json"));
+    //logs note to be deleted
+    console.log(selectedNote);
+
+    //gets old inputs
+
+    let db = require(path.join(__dirname, 'Develop/db/db.json'));
+    
+    //loop to remove the note from the array    
+    for (let i = 0; i < db.length; i++) {
+        if (selectedNote == db[i].id) {
+            console.log(db[i]);
+            db.splice(i, 1);
+        }
+    }
+
+    //shows current notes to console
+    console.log(db);
+
+    //exchanges data to server
+    const writeFile = JSON.stringify(db, null, 2);
+
+    //'refreshes' array
+    deletedNotes(writeFile);
+    //reloads notes
+    return res.json(db);
 });
 
-//route to the "notes" page
-//
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
-});
+//recreates the db file after adding a note
+function generateNotes(notes) {
+    fs.writeFile(path.join(__dirname, 'Develop/db/db.json'), notes, (err) =>
+        err ? console.error(err) : console.log('New note added successfully!'));
+}
 
-//Basic route that sends the user to the home (AJAX) page
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+//recreates the db file after removing a note
+ function deletedNotes(notes) {
+    fs.writeFile(path.join(__dirname, 'Develop/db/db.json'), notes, (err) =>
+         err ? console.error(err) : console.log('Note deleted successfully!'));
+ }
 
-//set the home page as the default route
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-//Starts the server to being listening
-//has to be last
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+//Starts the server to begin listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
